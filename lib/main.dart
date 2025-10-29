b1fcfd518710253563a41a783d7132c66ea5d242
JOR3NTO <jorgeedcobo@gmail.com>
2025-10-28 19:28:08 -0500

    Taller JWT

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'routes/app_router.dart';
 import 'themes/app_theme.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
 
 Future<void> main() async {
-  // Asegurarse de que los widgets de Flutter estén inicializados
   WidgetsFlutterBinding.ensureInitialized();
-  // Optimizar la carga del .env
   try {
     await dotenv.load(fileName: ".env");
   } catch (e) {
     debugPrint('Error loading .env file: $e');
   }
-  // Inicializar dotenv para cargar las variables de entorno
-  // await dotenv.load(fileName: ".env");
-
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()..loadSession()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,11 +27,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // go_router para navegación
     return MaterialApp.router(
-      theme: AppTheme.lightTheme, // Tema personalizado global
+      theme: AppTheme.lightTheme,
       title: 'Flutter - UCEVA',
-      routerConfig: appRouter, // Configuración de rutas con go_router
+      routerConfig: appRouter,
     );
   }
 }
